07944eb93ae3c75c530f819e5abfc4da9f4d4d1a
FrankWangJQ <32088064+FrankWangJQ@users.noreply.github.com>
2018-07-12 14:34:10 +0800

    jq

--- TestBegin.py
+++ TestBegin.py
@@ -7,5 +7,5 @@ kwargs = {
     #"dot_env_path": "/path/to/.env"
 }
 runner = HttpRunner(**kwargs)
-
+#入口
 runner.run("/Users/wangjianqing/PycharmProjects/HttpRunner-master/tests/testcases/Test/T_Login.yml")
