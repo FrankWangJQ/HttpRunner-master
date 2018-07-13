900518b6c7d2b954dda4b79afd65799729bd2768
FrankWangJQ <32088064+FrankWangJQ@users.noreply.github.com>
2018-07-13 14:44:56 +0800

    newReport

--- TestBegin.py
+++ TestBegin.py
@@ -9,3 +9,5 @@ kwargs = {
 runner = HttpRunner(**kwargs)
 
 runner.run("/Users/wangjianqing/PycharmProjects/HttpRunner-master/tests/testcases/Test/T_Login.yml")
+
+runner.gen_html_report(html_report_name="reportTest",html_report_template="/Users/wangjianqing/PycharmProjects/HttpRunner-master/httprunner/templates/default_report_template.html")
